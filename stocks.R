a3d9e1421aef04ea30bfc8fec6795768060af62d
JJXW <jonathan352@gmail.com>
2017-01-22 23:05:10 -0800

    yearly buy high sell low
    analysis on averatge yearly returns if you buy during the worst month in
    a year (highest price) and sell during the worst month of the most
    recent year (lowest price)

--- stocks.R
+++ stocks.R
@@ -29,6 +29,34 @@ ggplot(sp500_act,aes(x=Date,y=real_return),na.rm=T)+
        x="Month",
        y="Cash Multiplier (After Inflation and Dividends)")
 
+#yearly scenarios
+
+sp500_yearly <- sp500_act %>% 
+  group_by(year) %>%
+  summarize(year_price_high = max(real_return, na.rm=T),
+            year_price_low = min(real_return, na.rm=T),
+            year_price_avg = mean(real_return, na.rm=T)) %>%
+  mutate(join_key = 1)
+
+sp500_max <- sp500_yearly %>%
+  filter(year == max(year)) %>%
+  rename( max_price_high = year_price_high, 
+          max_price_low = year_price_low,
+          max_price_avg = year_price_avg,
+          max_year = year)
+
+sp500_yearly <- sp500_yearly %>%
+  left_join(sp500_max, by = c('join_key')) %>%
+  mutate(buy_high_sell_low_return = max_price_low / year_price_high,
+         buy_high_sell_low_yearly_return = (max_price_low / year_price_high) ^ (1/(max_year - year)) - 1) %>%
+  filter(year < max(year))
+
+ggplot(sp500_yearly,aes(x=year,y=buy_high_sell_low_yearly_return),na.rm=T)+
+  geom_path(color="black",alpha=1)+
+  labs(title="Returns After Investing",
+       x="Year Invested",
+       y="Yearly Average Return (After Inflation and Dividends)")
+
 #Calculate return for perfect market timing one buy one sell
 
 sp500_min <- min(sp500_act$real_return, na.rm=T) 
